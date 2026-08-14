13589e036162ada5f53c10f41eef00464084bb36
yuvpra1 <ryuvrajpratap@gmail.com>
2026-08-14 20:06:19 +0530

    Remove edge runtime config from proxy.ts

--- proxy.ts
+++ proxy.ts
@@ -1,8 +1,6 @@
 import { NextResponse } from 'next/server';
 import type { NextRequest } from 'next/server';
 
-export const runtime = 'edge';
-
 export default async function proxy(request: NextRequest) {
   const { pathname } = request.nextUrl;
   
